981f16ff46dbab6436f83205109d012f67c8f949
JonatanFD <u20211b995@upc.edu.pe>
2025-11-26 23:19:05 -0500

    dhcp ica configurado

--- comandos-ica.sh
+++ comandos-ica.sh
@@ -105,3 +105,19 @@ interface g0/0
  exit
 
 ip route 172.28.48.0 255.255.252.0 172.28.36.5
+
+
+# configuracion del dhcp para conectar las redes de wifi
+# switch core
+! --- WIFI EJECUTIVOS (VLAN 80) ---
+interface vlan 80
+ ip helper-address 172.28.49.44
+ no shutdown
+ exit
+
+! --- WIFI CLIENTES (VLAN 60) ---
+interface vlan 60
+ description Gateway Wifi Clientes
+ ip helper-address 172.28.49.44
+ no shutdown
+ exit
--- servicios-web.sh
+++ servicios-web.sh
@@ -3,3 +3,10 @@
 # Agregar el firewall para que acepte unicamente la red local todo menos en lima que accede todos
 
 # Configurar DHCP
+# Configuramos la pool de direcciones en dhcp
+# basicamente le estamos diciendo que si alguien pide una direccion ip entonces, elige una disponible nada mas.
+# aca influye mucho la configuracion de la vlan, los dispositivos no tiene ip asi que la forma de redirigir es
+# mediante la vlan
+
+
+
