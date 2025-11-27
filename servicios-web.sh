b65f15a504b032261f5a74540588e9b13e925b72
JonatanFD <u20211b995@upc.edu.pe>
2025-11-26 22:10:15 -0500

    configuracion de dhcp completada para la libertad, unicamente wifi clientes y ejecutivos estan asociados a dhcp

---
+++ servicios-web.sh
@@ -0,0 +1,5 @@
+# Configurar FTP
+# crear usuario y contra con permisos para cada vlan
+# Agregar el firewall para que acepte unicamente la red local todo menos en lima que accede todos
+
+# Configurar DHCP
