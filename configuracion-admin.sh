b5e741c6526747398dda627c196c47df90940c9b
JonatanFD <u20211b995@upc.edu.pe>
2025-11-29 15:34:54 -0500

    ica configurado con ssh

---
+++ configuracion-admin.sh
@@ -0,0 +1,42 @@
+# SSH
+# router
+# SW1, SW2, SW3, D1, D2, D3, SW Core, Router
+# PC ADMIN 172.28.42.104 LIMA
+# ACL para aceptar solamente pc admin
+en
+conf t
+ip access-list standard FILTRO_GESTION
+permit host 172.28.49.18
+deny any
+exit
+
+int vlan 99
+ip add 172.28.49.24 255.255.255.240
+no shutdown
+exit
+ip default-gateway 172.28.49.17
+
+hostname SW3_Ica
+ip domain-name miempresa.com
+username admin secret admin
+crypto key generate rsa
+1024
+line vty 0 4
+login local
+transport input ssh
+access-class FILTRO_GESTION in
+exit
+ip ssh version 2
+# comprobar
+show ip ssh
+
+# pc
+ssh -l admin <ip>
+
+# TELNET (APLICACION:23)
+# Switch
+# en secret cisco
+
+# line vty 0 4
+# password class
+# login
