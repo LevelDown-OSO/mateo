be622ed7aae29b7c91b63f82f7508b916ec24d37
JonatanFD <u20211b995@upc.edu.pe>
2025-11-29 01:29:02 -0500

    networking con internet

--- configuracion-internet.sh
+++ configuracion-internet.sh
@@ -1,13 +1,13 @@
 # 1. Asignacion de ip PARA EL GRUPO 2  fue     103.24.158.0/24
 # 2. conexion de switch con vlan
 
-en
-conf t
+# ISP1 -> ISP3
+ip route 103.24.158.0 255.255.255.0 219.0.0.5
+# ISP2 -> ISP3
+ip route 103.24.158.0 255.255.255.0 219.0.0.9
 
-vlan 10
-name DNS
-exit
 
-vlan 20
-name WEB
-exit
+# isp 3 -> isp 1
+ip route 0.0.0.0 0.0.0.0 219.0.0.6
+# isp 3 -> isp 2
+ip route 0.0.0.0 0.0.0.0 219.0.0.10
